ec4dcbfc70bbb7d7005eabfbc6ddd7258265889e
Anyushka Corporan <anyushka.corporan@gmail.com>
2026-08-18 10:08:13 -0400

    Clean up repo: remove loose build/helper scripts and orphan files

--- debug-comments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/Desktop/FaithFinderApp
-
-python3 << 'PYEOF'
-content = open('app/(tabs)/community.tsx').read()
-# Find the comments modal ScrollView
-idx = content.find('showComments')
-print(repr(content[idx:idx+2000]))
-PYEOF
